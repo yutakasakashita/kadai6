0510b2733dbb1c677b0715ff5e28183651df1edd
unknown <mpwsd518@gmail.com>
2024-06-10 23:24:55 +0900

    不要な差分を削除、パス名を複数形に変更

--- src/main/java/com/practise/Dog/DogApplication.java
+++ src/main/java/com/practise/Dog/DogApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DogApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(DogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DogApplication.class, args);
+    }
 
 }
--- src/main/java/com/practise/Dog/Dogcontroller.java
+++ src/main/java/com/practise/Dog/Dogcontroller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Dogcontroller {
 
-    @GetMapping("/dog")
+    @GetMapping("/dogs")
     public Dog Introduction() {
         return new Dog("pochi", 5, "Chihuahua");
     }
